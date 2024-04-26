104d20d6d7e115aae9943e1c7b9338d5c19c28e2
Yotty0404 <tyuke9216@hogmail.co.jp>
2024-04-27 02:32:01 +0900

    コンポーネントある程度動くように

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import './App.css';
 import React from 'react';
 import Modal from 'react-modal';
 import { Member } from './Member';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [memberList, setMemberList] = useState([]);
+
+  function setList() {
+    var memberList = JSON.parse(localStorage.getItem("member"));
+    if (memberList != null) {
+      setMemberList(memberList);
+    }
+  }
+
+  useEffect(() => {
+    setList();
+  }, []);
 
   function openModal() {
     console.log("click2")
@@ -14,45 +27,41 @@ function App() {
   }
 
   function closeModal() {
-    console.log("clickclose")
     setIsOpen(false);
+    setList();
   }
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>test0426Update02</div>
+    <div id="container">
+      <div id="header"></div>
+
+      <div id="main">
+        <div>test0426Update03</div>
 
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>Click me!</button>
 
         <h1 className="text-3xl font-bold underline">
           Hello Tailwind CSS!
         </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
+        {memberList.map((member, index) =>
+          <div className="flex">
+            <div>{member.name}</div>
+          </div>)}
+
+
+      </div>
 
       <Modal
         isOpen={modalIsOpen}
-        onRequestClose={closeModal}
         contentLabel="modal"
       >
         <div >
           <button onClick={closeModal} className="i-lucide-x relative -top-3 -right-3 float-right font-bolder text-4xl hover:bg-gray-500"></button>
           <h2 className="text-center text-xl">設定</h2>
-          <Member />
+          <Member isMember={true} />
+          <Member isMember={false} />
         </div>
       </Modal>
     </div>
--- src/Member.js
+++ src/Member.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
-function Member() {
+function Member(prop) {
     const [memberName, setMemberName] = useState("");
-    let [memberList, setMemberList] = useState([]);
+    const [memberList, setMemberList] = useState([]);
+
+    useEffect(() => {
+
+        var list = JSON.parse(localStorage.getItem(prop.isMember ? "member" : "item"));
+        console.log(list)
+        if(list == null) return;
+        setMemberList(list)
+
+    }, []);
+
 
     function addMember() {
         if (memberName === "") {
-            console.log("empty")
         }
         else {
-            console.log(memberList);
-            console.log(Math.max.apply(null, memberList.map((x) => { return x.id })));
-
             var id = 0;
 
             if (memberList.length > 0) {
@@ -24,22 +31,41 @@ function Member() {
             }
             memberList.push(newMember);
             setMemberName("");
+
+            localStorage.setItem(prop.isMember ? "member" : "item", JSON.stringify(memberList));
         }
     }
 
-
     function deleteMember(e) {
         var removeId = e.target.parentNode.querySelector('input').id;
-        setMemberList([...memberList.filter(member => member.id != removeId)])
+        var newMemberList = [...memberList.filter(member => member.id != removeId)];
+        setMemberList(newMemberList)
 
+        localStorage.setItem(prop.isMember ? "member" : "item", JSON.stringify(newMemberList));
     }
+
+    const handleInputChange = (index, value) => {
+        const newMemberList = [...memberList];
+        newMemberList[index].name = value;
+        setMemberList(newMemberList);
+
+        localStorage.setItem(prop.isMember ? "member" : "item", JSON.stringify(newMemberList));
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            var button = e.target.parentNode.querySelector('button');
+            button.click();
+        }
+    };
 
     return (
         <div>
             <div className="flex flex-col gap-2">
-                {memberList.map(member =>
+                {memberList.map((member, index) =>
                     <div className="flex">
                         <input id={member.id}
+                            onChange={(e) => handleInputChange(index, e.target.value)}
                             value={member.name}
                             className="h-8 w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none"
                         ></input>
@@ -49,6 +75,7 @@ function Member() {
                     <input
                         value={memberName}
                         onChange={(e) => setMemberName(e.target.value)}
+                        onKeyDown={handleKeyDown}
                         className="h-8 w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:outline-none" />
                     <button className="i-lucide-plus h-8 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded" onClick={addMember}></button>
                 </div>
